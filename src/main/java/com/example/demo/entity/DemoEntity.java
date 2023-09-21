6e065501f34239bae12a0fa088af52d24be11238
songdobest <dlgusdn627@naver.com>
2023-09-21 16:44:29 +0900

    스프링부트와 데이터베이스연결

---
+++ src/main/java/com/example/demo/entity/DemoEntity.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+public class DemoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+
+}
